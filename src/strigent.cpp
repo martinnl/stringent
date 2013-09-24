4383216f151de47c6818093ff8f5d9818a399464
martinnl <martin.nielsen.lonn@gmail.com>
2013-09-25 00:46:43 +0200

    Initial commit

---
+++ src/strigent.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : strigent.cpp
+ Author      : Martin Nielsen-Lönn
+ Version     :
+ Copyright   : GPL
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
